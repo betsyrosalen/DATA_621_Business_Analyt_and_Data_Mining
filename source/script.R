e15d1a751df292af0526319dabb78a42717b7c52
Betsy Rosalen <betsy@mylittleuniverse.com>
2019-02-12 16:11:40 -0500

    Updated templates for report in two formats w/ Gabby's code - Added csv files

--- source/script.R
+++
@@ -1,7 +0,0 @@
-mtcars2 <- mtcars
-mtcars2$am <- factor(
-  mtcars$am, labels = c('automatic', 'manual')
-)
-graph1 <- ggplot(mtcars2, aes(hp, mpg, color = am)) +
-  geom_point() + geom_smooth() +
-  theme(legend.position = 'bottom')

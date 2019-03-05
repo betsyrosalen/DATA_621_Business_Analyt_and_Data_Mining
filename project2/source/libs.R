e4d741eeac6196fc02043751cf1b34f121dda548
silverrainb <62fb1d0a@opayq.com>
2019-03-05 14:44:47 +0900

    changing the format, adding script files

---
+++ project2/source/libs.R
@@ -0,0 +1,4 @@
+if (!require('data.table')) (install.packages('data.table'))
+if (!require('ggplot2')) (install.packages('ggplot2'))
+if (!require('caret')) (install.packages('caret'))
+if (!require('pROC')) (install.packages('pROC'))

8890de716003d68e39c4463fc2f4ecbc85f1fd0c
Betsy Rosalen <betsy@mylittleuniverse.com>
2019-04-09 23:53:28 -0400

    Update libs.R

--- project3_crime/source/libs.R
+++ project3_crime/source/libs.R
@@ -1,4 +1,4 @@
-if (!require('alr3')) (install.packages('alr3'))
+if (!require('car')) (install.packages('car'))
 if (!require('data.table')) (install.packages('data.table'))
 if (!require('ggplot2')) (install.packages('ggplot2'))
 if (!require('caret')) (install.packages('caret'))

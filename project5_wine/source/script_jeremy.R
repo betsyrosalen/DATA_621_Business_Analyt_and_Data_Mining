b13f8e922cda9347681caba7e035d318c5a00b27
JeremyOBrien16 <35013907+JeremyOBrien16@users.noreply.github.com>
2019-05-10 15:02:27 -0400

    made updates to data preparation section, some work still in progress

--- project5_wine/source/script_jeremy.R
+++ project5_wine/source/script_jeremy.R
@@ -1,12 +1,10 @@
-library(stats)
-
 set.seed(123)
 
-impute.data <- mice(train, m = 5, maxit = 5, print = FALSE)
+impute.data <- mice::mice(train, m = 5, maxit = 5, print = FALSE)
 
 density.plot <- densityplot(impute.data)
 
-train_scaled <- train %>% 
+train_for_scaling <- train %>% 
   dplyr::select(Alcohol,
                 CitricAcid, 
                 FixedAcidity, 
@@ -16,14 +14,23 @@ train_scaled <- train %>%
                 TotalSulfurDioxide, 
                 VolatileAcidity)
 
-  
 positive_scale <- function(x) {
-  lower_bound <- mean(x) - (stats::IQR(train$FixedAcidity) * 1.5)
-  x <- x + lower_bound
-  if(x > 0) {
-    x } else {
-     0 
-    }
+  low_bound <- mean(x, na.rm = TRUE) - stats::IQR(x, na.rm = TRUE) * 1.5
+  if(x < low_bound) {
+    x = 0
+  } else {
+    x = x + abs(low_bound) 
+  }
 }
   
-lapply(train_scaled, positive_scale)
+train_scaled <- lapply(train_for_scaling, positive_scale) %>% 
+  as.data.frame()
+
+hist_lt_scaled <- train_scaled %>%
+  gather() %>%
+  ggplot(aes(value)) +
+  facet_wrap(~ key, scales = "free") +
+  geom_histogram(fill = "#58BFFF") +
+  xlab("") +
+  ylab("") +
+  theme(panel.background = element_blank())

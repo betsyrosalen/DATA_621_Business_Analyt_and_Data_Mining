4dc9f0bcdd0c43711cedb967e2cf7a714eb7897f
JeremyOBrien16 <35013907+JeremyOBrien16@users.noreply.github.com>
2019-05-11 15:39:48 -0400

    tidied code so train_imputed includes scaled values, additive scaling function still needs work

--- project5_wine/source/script_jeremy.R
+++ project5_wine/source/script_jeremy.R
@@ -1,15 +1,17 @@
-set.seed(123)
 
+# impute NAs using MICE
 train_mice <- mice::mice(train, m = 5, maxit = 5, print = FALSE)
 train_imputed <- mice::complete(train_mice)
 
+# confirm no additional NAs
 #train_imputed %>% 
 #is.na() %>% 
 #colSums()
 
 density.plot <- densityplot(train_mice)
 
-train_for_scaling <- train_imputed %>% 
+# subset and rename variables with count values for concentrations / amounts that are < 0 
+train_scaling_subset <- train_imputed %>% 
   dplyr::select(Alcohol,
                 CitricAcid, 
                 FixedAcidity, 
@@ -17,8 +19,11 @@ train_for_scaling <- train_imputed %>%
                 ResidualSugar, 
                 Sulphates, 
                 TotalSulfurDioxide, 
-                VolatileAcidity)
+                VolatileAcidity) %>% 
+  dplyr::rename_all(paste0, '_addscaled')
 
+# function intended to additively scale values by amount equivalent to lower bound of 1.5 * IQR, then drop anything below 0 
+# not working as intended
 positive_scale <- function(x) {
   low_bound <- mean(x, na.rm = TRUE) - (stats::IQR(x, na.rm = TRUE) * .5) * 1.5
   if(is.na(x)) {
@@ -29,9 +34,20 @@ positive_scale <- function(x) {
     x = x + abs(low_bound) 
   }
 }
-  
-train_scaled <- lapply(train_for_scaling, positive_scale) %>% 
+
+# rescale subset of variables with values < 0
+train_scaled_subset <- lapply(train_scaling_subset, positive_scale) %>% 
   as.data.frame()
+
+# reattach index for join with other variables
+train_scaled_subset <- cbind(train_imputed$`ï..INDEX`, train_scaled_subset)
+names(train_scaled_subset)[1] <- 'ï..INDEX'
+
+# combined rescaled variables with original frame, marked as '_addscaled'
+train_imputed <- train_imputed %>% 
+  left_join(train_scaled_subset, by = 'ï..INDEX')
+
+train_imputed$STARS <- as.factor(train_imputed$STARS)
 
 #hist_lt_scaled <- train_scaled %>%
   #gather() %>%

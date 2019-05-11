5cb5ffe951ec3f878672a5be402ff24cc5d13ef6
JeremyOBrien16 <35013907+JeremyOBrien16@users.noreply.github.com>
2019-05-11 12:57:04 -0400

    updated mice imputation, additive scaling function still throwing errors

--- project5_wine/source/script_jeremy.R
+++ project5_wine/source/script_jeremy.R
@@ -1,10 +1,15 @@
 set.seed(123)
 
-impute.data <- mice::mice(train, m = 5, maxit = 5, print = FALSE)
+train_mice <- mice::mice(train, m = 5, maxit = 5, print = FALSE)
+train_imputed <- mice::complete(train_mice)
 
-density.plot <- densityplot(impute.data)
+#train_imputed %>% 
+#is.na() %>% 
+#colSums()
 
-train_for_scaling <- train %>% 
+density.plot <- densityplot(train_mice)
+
+train_for_scaling <- train_imputed %>% 
   dplyr::select(Alcohol,
                 CitricAcid, 
                 FixedAcidity, 
@@ -15,8 +20,10 @@ train_for_scaling <- train %>%
                 VolatileAcidity)
 
 positive_scale <- function(x) {
-  low_bound <- mean(x, na.rm = TRUE) - stats::IQR(x, na.rm = TRUE) * 1.5
-  if(x < low_bound) {
+  low_bound <- mean(x, na.rm = TRUE) - (stats::IQR(x, na.rm = TRUE) * .5) * 1.5
+  if(is.na(x)) {
+    x = NA
+  } else if(x < low_bound) {
     x = 0
   } else {
     x = x + abs(low_bound) 
@@ -26,11 +33,11 @@ positive_scale <- function(x) {
 train_scaled <- lapply(train_for_scaling, positive_scale) %>% 
   as.data.frame()
 
-hist_lt_scaled <- train_scaled %>%
-  gather() %>%
-  ggplot(aes(value)) +
-  facet_wrap(~ key, scales = "free") +
-  geom_histogram(fill = "#58BFFF") +
-  xlab("") +
-  ylab("") +
-  theme(panel.background = element_blank())
+#hist_lt_scaled <- train_scaled %>%
+  #gather() %>%
+  #ggplot(aes(value)) +
+  #facet_wrap(~ key, scales = "free") +
+  #geom_histogram(fill = "#58BFFF") +
+  #xlab("") +
+  #ylab("") +
+  #theme(panel.background = element_blank())
